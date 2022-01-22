6c6f0b1130b3fa5686a152630d5fd25d96dd913d
Electro Games <2cool4u567@gmail.com>
2022-01-21 23:44:06 -0600

    OOps

--- src/Electro/DeathMoney/DeathMoney.php
+++ src/Electro/DeathMoney/DeathMoney.php
@@ -29,30 +29,30 @@ class DeathMoney extends PluginBase implements Listener{
         {
             $damager = $event->getDamager();
             if (!$damager instanceof Player) return;
-        }
-        if ($this->getConfig()->get("Type") == "all"){
-            $player->sendMessage("§aYou have died and lost $" . $playerMoney);
-            $damager->sendMessage("§aYou have killed " . $player->getName() . " and stole $" . $playerMoney);
-            EconomyAPI::getInstance()->addMoney($damager, $playerMoney);
-            EconomyAPI::getInstance()->reduceMoney($player, $playerMoney);
-        }
-        if ($this->getConfig()->get("Type") == "half"){
-            $player->sendMessage("§aYou have died and lost $" . $playerMoney / 2);
-            $damager->sendMessage("§aYou have killed " . $player->getName() . " and stole $" . $playerMoney / 2);
-            EconomyAPI::getInstance()->addMoney($damager, $playerMoney / 2);
-            EconomyAPI::getInstance()->reduceMoney($player, $playerMoney / 2);
-        }
-        if ($this->getConfig()->get("Type") == "amount"){
-            $player->sendMessage("§aYou have died and lost $" . (double)$this->getConfig()->get("Money-Loss"));
-            $damager->sendMessage("§aYou have killed " . $player->getName() . " and stole $" . (double)$this->getConfig()->get("Money-Loss"));
-            EconomyAPI::getInstance()->addMoney($damager, (double)$this->getConfig()->get("Money-Loss"));
-            EconomyAPI::getInstance()->reduceMoney($player, (double)$this->getConfig()->get("Money-Loss"));
-        }
-        if ($this->getConfig()->get("Type") == "percent"){
-            $player->sendMessage("§aYou have died and lost $" . ((double)$this->getConfig()->get("Money-Loss") / 100) * $playerMoney);
-            $damager->sendMessage("§aYou have killed " . $player->getName() . " and stole $" . ((double)$this->getConfig()->get("Money-Loss") / 100) * $playerMoney);
-            EconomyAPI::getInstance()->addMoney($damager, ((double)$this->getConfig()->get("Money-Loss") / 100) * $playerMoney);
-            EconomyAPI::getInstance()->reduceMoney($player, ((double)$this->getConfig()->get("Money-Loss") / 100) * $playerMoney);
+            if ($this->getConfig()->get("Type") == "all"){
+                $player->sendMessage("§aYou have died and lost $" . $playerMoney);
+                $damager->sendMessage("§aYou have killed " . $player->getName() . " and stole $" . $playerMoney);
+                EconomyAPI::getInstance()->addMoney($damager, $playerMoney);
+                EconomyAPI::getInstance()->reduceMoney($player, $playerMoney);
+            }
+            if ($this->getConfig()->get("Type") == "half"){
+                $player->sendMessage("§aYou have died and lost $" . $playerMoney / 2);
+                $damager->sendMessage("§aYou have killed " . $player->getName() . " and stole $" . $playerMoney / 2);
+                EconomyAPI::getInstance()->addMoney($damager, $playerMoney / 2);
+                EconomyAPI::getInstance()->reduceMoney($player, $playerMoney / 2);
+            }
+            if ($this->getConfig()->get("Type") == "amount"){
+                $player->sendMessage("§aYou have died and lost $" . (double)$this->getConfig()->get("Money-Loss"));
+                $damager->sendMessage("§aYou have killed " . $player->getName() . " and stole $" . (double)$this->getConfig()->get("Money-Loss"));
+                EconomyAPI::getInstance()->addMoney($damager, (double)$this->getConfig()->get("Money-Loss"));
+                EconomyAPI::getInstance()->reduceMoney($player, (double)$this->getConfig()->get("Money-Loss"));
+            }
+            if ($this->getConfig()->get("Type") == "percent"){
+                $player->sendMessage("§aYou have died and lost $" . ((double)$this->getConfig()->get("Money-Loss") / 100) * $playerMoney);
+                $damager->sendMessage("§aYou have killed " . $player->getName() . " and stole $" . ((double)$this->getConfig()->get("Money-Loss") / 100) * $playerMoney);
+                EconomyAPI::getInstance()->addMoney($damager, ((double)$this->getConfig()->get("Money-Loss") / 100) * $playerMoney);
+                EconomyAPI::getInstance()->reduceMoney($player, ((double)$this->getConfig()->get("Money-Loss") / 100) * $playerMoney);
+            }
         }
     }
 }
